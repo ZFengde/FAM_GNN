177f3d6ef220f946b8736d612543435910eb8a41
Feng Zhang <feng.1.zhang@kcl.ac.uk>
2023-03-27 11:00:05 +0100

    update

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ if __name__ == '__main__':
     parser.add_argument('--env_id', type=str, default='Turtlebot-v2') # 'Turtlebot-v2''Safexp-PointGoal1-v0'
     parser.add_argument('--algo', type=str, default='Temp_GNN_PPO') 
     parser.add_argument('--policy_type', type=str, default='MlpPolicy')
-    parser.add_argument('--n_envs', type=int, default=1)
+    parser.add_argument('--n_envs', type=int, default=4)
     parser.add_argument('--iter_num', type=int, default=2000) # Total_timestep = iter_num * n_envs * n_steps, here is 2000 * 3 * 2048 = 1.2e7
     parser.add_argument('--seed', type=int, default=0)
     parser.add_argument('--net_arch_dim', type=int, default=64)
