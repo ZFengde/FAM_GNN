18fdc35f4bcdb6eff123e07a54555ddbe2d804db
Feng Zhang <feng.1.zhang@kcl.ac.uk>
2023-04-02 11:48:18 +0100

    update

--- fam_gnn/common/fam_gnn.py
+++ fam_gnn/common/fam_gnn.py
@@ -171,7 +171,8 @@ class FAM_GNN(nn.Module):
         x = th.tanh(self.layer1(g, feat, etypes, ntypes, attention))
         x = th.tanh(self.layer2(g, x, etypes, ntypes, attention)) # node_num, batch, out_dim
         # here we take robot, target, and a compressed obstacle info
-        x = th.stack((x[0], x[1], th.max(x[2:], dim=0).values, th.min(x[2:], dim=0).values, th.mean(x[2:], dim=0)), dim=0)
+        # x = th.stack((x[0], x[1], th.max(x[2:], dim=0).values, th.min(x[2:], dim=0).values, th.mean(x[2:], dim=0)), dim=0)
+        x = x[0].unsqueeze(0)
         return x
 
 class FAM_GNNLayer_noatte(nn.Module): # using antecedants to update node features
