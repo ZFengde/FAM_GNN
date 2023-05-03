ce9796f89ee55921660bb48c31c7969f3431e8ec
Feng Zhang <feng.1.zhang@kcl.ac.uk>
2023-05-03 23:01:54 +0100

    update

--- main.py
+++ main.py
@@ -69,8 +69,8 @@ def main(
 
 	env = make_vec_env(env_id, n_envs=n_envs, vec_env_cls=SubprocVecEnv, env_kwargs=env_kwargs)
 	# make experiment directory
-	logdir = f"{env_id}+n_obstalces={obstacle_num}/{log_name+str(indicator)}/logs/{int(time.time())}/"
-	modeldir = f"{env_id}+n_obstalces={obstacle_num}/{log_name+str(indicator)}/models/{int(time.time())}/"
+	logdir = f"{env_id}+n_obstalces={obstacle_num}/{log_name+str(seed)}/logs/{int(time.time())}/"
+	modeldir = f"{env_id}+n_obstalces={obstacle_num}/{log_name+str(seed)}/models/{int(time.time())}/"
 
 	if not os.path.exists(modeldir):
 		os.makedirs(modeldir)
@@ -78,7 +78,7 @@ def main(
 		os.makedirs(logdir)
 
 	if early_stop:
-		target_kl = 0.02
+		target_kl = 0.01
 	else:
 		target_kl = None
 	model = algo(
@@ -107,7 +107,7 @@ if __name__ == '__main__':
     parser.add_argument('--seed', type=int, default=30)
     parser.add_argument('--net_arch_dim', type=int, default=64)
     parser.add_argument('--obstacle_num', type=int, default=7)
-    parser.add_argument('--indicator', type=int, default=1)
+    parser.add_argument('--indicator', type=int, default=2)
     parser.add_argument('--gnn_type', type=str, default='fam_gnn') 
     # fam_gnn, fam_gnn_noatte, gat, rel_gcn, fam_rel_gcn | temp_fam_gnn, temp_fam_rel_gcn
     parser.add_argument('--early_stop', action='store_true') # if no action, or said default if False, otherwise it's True
